e2da1b2b54bde01ec7559d38b791a8a6aa449dd5
hideo <sankamonohideo@sankamoeiseinoMacBook-Pro.local>
2018-01-21 12:23:46 +0900

    for change encode

--- csvanketo1.py
+++ csvanketo1.py
@@ -1,8 +1,12 @@
 import csv
-CsvFile = csv.reader(open('anketo1.csv'),delimiter=',')
+CsvFile = csv.reader(open('anketo1e.csv'),delimiter=',')
 CsvList = []
 
 for i in CsvFile:
     CsvList.append(i)
 
 print len(CsvList)
+for i in range(0,10):
+    if len(CsvList[i][5]) != 0:
+        print len(CsvList[i][5])
+
